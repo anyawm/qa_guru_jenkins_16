94135f1ca5590ac0bdc8b251aaa1613f249d9df8
Anna Petrova <mail.anna.p@yandex.ru>
2023-01-07 13:43:34 +0500

    add attachments

--- src/test/java/stud/anna/TestBase.java
+++ src/test/java/stud/anna/TestBase.java
@@ -2,18 +2,24 @@ package stud.anna;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true)
+        Configuration.browserCapabilities = capabilities;
     }
 
     @BeforeEach
@@ -23,9 +29,9 @@ public class TestBase {
 
     @AfterEach
     void addAttachments() {
-        // screenshot
-        // page source
-        // console logs
-        // video
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
